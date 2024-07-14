85e824ddb4e111110332f80a875751990577df0e
Madhusudhana, Rochan Honnuduke <rochan.hm@gatech.edu>
2024-07-15 01:23:12 +0530

    Add download and upload button

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 
 import gradio as gr
 import httpx
@@ -7,6 +8,7 @@ if not os.getenv("MCM_URL"):
     raise ValueError("Please set the MCM_URL environment variable")
 
 csv_logger = gr.CSVLogger()
+csv_path = pathlib.Path("flagged/log.csv")
 
 
 def get_mcm_response(question: str) -> str:
@@ -77,6 +79,10 @@ with gr.Blocks() as demo:
     with gr.Row():
         # Flag
         flag_btn = gr.Button(value="Save For Review", variant="secondary")
+        # Download
+        download_btn = gr.DownloadButton(
+            label="Download Logs", value=csv_path, visible=True
+        )
 
     csv_logger.setup([question, output], "flagged")
 
@@ -87,10 +93,21 @@ with gr.Blocks() as demo:
     def on_clear_click():
         return "", ""
 
+    def on_flag_click(*args):
+        if not args[0] and not args[1]:
+            gr.Warning("No data to save!")
+            return
+
+        csv_logger.flag(args)
+        gr.Info("Saved successfully!")
+
     submit_button.click(on_submit_click, inputs=[question], outputs=[output])
     clear_button.click(on_clear_click, outputs=[question, output])
     flag_btn.click(
-        lambda *args: csv_logger.flag(args), [question, output], None, preprocess=False
+        on_flag_click,
+        [question, output],
+        None,
+        preprocess=False,
     )
 
 demo.launch()
